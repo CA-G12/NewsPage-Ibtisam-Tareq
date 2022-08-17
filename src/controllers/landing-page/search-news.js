369b488a39d7aa9e1c7b8896da402b3d1164d1ce
Ibtisam-Hemmo <ibtisamhemmo@gmail.com>
2022-08-17 16:07:56 +0300

    fix bugs

--- src/controllers/landing-page/search-news.js
+++ src/controllers/landing-page/search-news.js
@@ -1,7 +1,7 @@
 const fetch = require('node-fetch')
 
 const searchNews=(req,res)=>{
-    fetch(`https://newsdata.io/api/1/news?apikey=pub_103021c47300bcc8aa250958a11ccf42eaa05&language=en&category=${req.params.news}`)
+    fetch(`https://newsdata.io/api/1/news?apikey=${process.env.API_KEY}&language=en&category=${req.params.news}`)
     .then(response => response.json())
     .then(response =>res.json(response))
     .catch(error => console.log(error));
