a8e0ee44c9d1f4e23f9c9946b56abd7f8e37747c
Ibtisam-Hemmo <ibtisamhemmo@gmail.com>
2022-08-17 10:42:36 +0300

    Fetching News and rendering them in the front end relates #7

--- public/JS/script.js
+++ public/JS/script.js
@@ -1,4 +1,19 @@
-fetch('/news').then((data) => data.json()).then((data) => console.log(data)).catch((err) => {
-    console.log(err);
-  });
-  
+const cards = document.querySelector('.cards');
+console.log(cards);
+
+fetch('/news')
+.then((data) => data.json())
+.then((data) => renderNews(data))
+.catch((err) => {console.log(err);});
+
+function renderNews(data) {
+  console.log(data);
+    cards.innerHTML = 
+    `<div class="card">
+      <img src=${data.results[0].image_url} alt="" />
+      <h4>${data.results[0].title}</h4>
+      <div class="action">
+          <a href=${data.results[0].link}>See More</a>
+      </div>
+    </div>`
+}
